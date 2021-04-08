3f050b9b882a57701168cc315175f6d0b89aea90
llasurd1 <llasurdo@gmail.com>
2021-04-07 21:15:26 -0400

    not finished but done

--- app/src/main/java/com/example/frogger/MainActivity.java
+++ app/src/main/java/com/example/frogger/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.frogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button play, instructions, leader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = findViewById(R.id.button_play);
+        instructions = findViewById(R.id.button_instructions);
+        leader = findViewById(R.id.button_leaderboards);
+    }
+    public void switch_activitesPlay(View view) {
+        Intent act_action =  new Intent(this, game.class);
+        startActivity(act_action);
+    }
+    public void switch_activitesInst(View view) {
+        Intent act_action =  new Intent(this, game.class);
+        startActivity(act_action);
+    }
+    public void switch_activitesLead(View view) {
+        Intent act_action =  new Intent(this, leaderboard.class);
+        startActivity(act_action);
     }
 }
--- app/src/main/java/com/example/frogger/leaderboard.java
+++ app/src/main/java/com/example/frogger/leaderboard.java
@@ -2,13 +2,24 @@ package com.example.frogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class leaderboard extends AppCompatActivity {
 
+    ListView lead;
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leaderboard);
+        back = findViewById(R.id.button_leaderboards_back);
+    }
+    public void switch_activites(View view) {
+        Intent act_action =  new Intent(this, MainActivity.class);
+        startActivity(act_action);
     }
 }
